5c51ba89c7337dd109245b2c1827c32662ca2c42
Fitri San <rushwender@gmail.com>
2019-08-02 01:38:51 +0800

    End of lesson 2

---
+++ Zlack/Controller/ChannelVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelVC.swift
+//  Zlack
+//
+//  Created by Piteli Chan on 8/2/19.
+//  Copyright © 2019 pitelichan. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Zlack/Controller/ChatVC.swift
@@ -0,0 +1,23 @@
+//
+//  ChatVC.swift
+//  Zlack
+//
+//  Created by Piteli Chan on 8/2/19.
+//  Copyright © 2019 pitelichan. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        // Do any additional setup after loading the view.
+    }
+
+}
