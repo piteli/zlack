88c27fe60184f3b974eb670981de8e6defbe292a
Fitri San <rushwender@gmail.com>
2019-08-16 23:37:37 +0800

    Lesson 11

--- Zlack/Controller/CreateAccountVC.swift
+++ Zlack/Controller/CreateAccountVC.swift
@@ -30,7 +30,12 @@ class CreateAccountVC: UIViewController {
         AuthService.instance.registerUser(email: email, password: pass){(success) in
             
             if(success){
-                print("Registered User!")
+                AuthService.instance.loginUser(email : email, password : pass, completion : {
+                    (success) in
+                    if success {
+                        print("Logged in USer!@", AuthService.instance.authToken)
+                    }
+                })
             }
         }
     }
--- Zlack/Services/AuthService.swift
+++ Zlack/Services/AuthService.swift
@@ -38,7 +38,7 @@ class AuthService {
         }
     }
     
-    func registerUser(email : String, password : String, _ : @escaping CompletionHandler){
+    func registerUser(email : String, password : String, completion : @escaping CompletionHandler){
         let lowerCaseEmail = email.lowercased()
         let header = [
             "Content-Type" : "application/json; charset=utf-8"
@@ -50,6 +50,42 @@ class AuthService {
         Alamofire.request(url : URL_REGISTER, method : .post, parameters : body, encoding : JSONEncoding.default, headers : header).responseString {
             (response) in
             if response.result.error == nil {
+                completion(true)
+            }else{
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+    func loginUser(email : String, password : String, completion : @escaping CompletionHandler){
+        let lowerCaseEmail = email.lowercased()
+        
+        let body : [String: Any] = [
+            "email" : lowerCaseEmail,
+            "password" : password
+        ]
+        
+        Alomofire.request(URL_LOGIN, method : .post, parameters : body, encoding : JSONEncoding.default, headers : HEADER).responseJSON {
+            (response) in
+            if response.result.error == nil{
+//                if let json = response.result.value as? Dictionary<String, Any>{
+//                    if let email = json["user"] as? String{
+//                        self.userEmail = email
+//                    }
+//                    if let token = json["token"] as? String{
+//                        self.authToken = token;
+//                    }
+//                }
+                
+                //using swiftyJSON
+                guard let data = response.data else{return}
+                let json = JSON(data : data)
+                self.userEmail = json["user"].stringValue //stringValue is.. in case, if user : nil, stringValue will auto set ""
+                self.authToken = json["token"].stringValue
+                
+                
+                self.isLoggedIn = true;
                 completion(true)
             }else{
                 completion(false)
