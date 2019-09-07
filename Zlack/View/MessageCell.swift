dd25b864db0fa3aab34c9c7595376a32d164614c
piteli <rushwender@gmail.com>
2019-09-08 07:26:12 +0800

    updated

--- Zlack/View/MessageCell.swift
+++ Zlack/View/MessageCell.swift
@@ -24,6 +24,20 @@ class MessageCell: UITableViewCell {
         usernameLbl.text = message.userName
         CircleImage.image = UIImage(named : message.userAvatar)
         CircleImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        //2017-07-13T21:49:25.590z
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy : -5)
+        isoDate = isoDate.substring(to : end)
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from : isoDate.appending("Z"))
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from  : finalDate)
+            timestampLbl.text = finalDate
+        }
     }
 
 }
